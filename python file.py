4831601df0ba925b37f7f5b689e6e01fcd04d636
akwuakazita-cmd <akwuakazita@gmail.com>
2026-03-06 12:38:52 +0100

    Add files via upload

---
+++ python file.py
@@ -0,0 +1,13 @@
+name= "Zita"
+
+Job="Data Analysts"
+
+Location= "Abuja"
+
+def addNos(a,b):
+    return f"sum of {a} and {b} is :{a+b}"
+
+    def subtractsNos(a,b):
+        """Returns the diffrence of two numbers"""
+        return a-b
+        
